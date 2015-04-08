4f8844cc7bb6267f0c0f48600c77286f5feba8c8
edmondmw <edmondmwang@gmail.com>
2015-04-08 03:10:51 -0700

    Up Arrow works
    Can go to previous command by pressing up arrow

--- ashell.cpp
+++ ashell.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <termios.h>
 #include <ctype.h>
+#include <list>
 
 using namespace std;
 
@@ -68,16 +69,67 @@ bool isDown(char c)
     return false;
 }
 
+/*void upCommand(const list<string> myList, list<string>::const_iterator &it, string &command, string &originalCommand)
+{
+    if(it == myList.end())
+    {
+        originalCommand = command;
+    }
+
+    it--;
+    //temporary for loop to clear the current line. Just print backspaces
+    for(int i = 0; i < command.length(); i++)
+    {
+        write(STDOUT_FILENO, "\b \b", 5);
+    }               
+    //write out the command
+    write(STDOUT_FILENO, it->c_str(), it->length());
+    command = *it;
+}
+
+void downCommand(const list<string> myList, list<string>::const_iterator &it, string &command, const string originalCommand)
+{
+    
+    it++;
+    //temporary for loop to clear the current line. Just print backspaces
+    for(int i = 0; i < command.length(); i++)
+    {
+        write(STDOUT_FILENO, "\b \b", 5);
+    }     
+
+    if(it == myList.end())
+    {
+        command = originalCommand;
+    }
+    else
+    {
+        command = *it;
+    }          
+    //write out the command
+    write(STDOUT_FILENO,command.c_str(), command.length());
+
+}*/
+
 int main()
 {
-    //string currentCommand;
     char character;
+    //the currentCommand being displayed
     string currentCommand;
-    struct termios savedTermAttributes;
+    //the command that was originally typed in. 
+    //Want to save this for when we use up/down arrows so we can return to the original command
+    string originalCommand;
+    //linked list of strigs that keeps track of previous commands
+    list<string> historyList;
+    //iterator to iterate through the historyList
+    list<string>::const_iterator it;
+    //set the iterator to the end of the list
+    it = historyList.end();
 
+    struct termios savedTermAttributes;
+    
     setNonCanonicalMode(STDIN_FILENO, &savedTermAttributes);
     
-    while(1)
+    while(true)
     {
         //read the user input
         read(STDIN_FILENO, &character, 1);
@@ -94,18 +146,57 @@ int main()
         else if(isUp(character))
         {
             //gotta clear screen and show the previous thing on the stack
+            //if the iterator is not at the beginning  and not empty then show the previous command
+            if(it != historyList.begin() && !historyList.empty() )
+            {
+                it--;
+                //temporary for loop to clear the current line. Just print backspaces
+                for(int i = 0; i < currentCommand.length(); i++)
+                {
+                    write(STDOUT_FILENO, "\b \b", 5);
+                }
+                
+                //write out the command
+                write(STDOUT_FILENO, it->c_str(), it->length());
+                currentCommand = *it;
+
+              //  upCommand(historyList, it, currentCommand, originalCommand);
+            }
         }    
 
         //if character is down arrow
         else if(isDown(character))
         {
-            //do down arrow stuff
+            if(it != historyList.end() && !historyList.empty())
+            {    
+               it++;
+                for(int i = 0; i < currentCommand.length(); i++)
+                {
+                    write(STDOUT_FILENO, "\b \b", 5);
+                }
+
+                //if iterator is at the end then we write out the original command
+                if(it == historyList.end())
+                {
+                    
+                }
+
+              //  write(STDOUT_FILENO, )
+                //cout<<endl<<"in  if down"<<endl;
+                //downCommand(historyList,it,currentCommand,originalCommand);
+            }
         }
 
         //if character is enter
         else if(character == 0x0A)
         {
-            //do enter stuff
+            //place the current command into the history list
+            historyList.push_back(currentCommand);
+ 
+            //enter has been typed so currentCommand needs to be reset
+            currentCommand.clear();
+
+            it = historyList.end();
         }
         //regular input, then just add the character to the command string and right it out
         else
@@ -114,7 +205,7 @@ int main()
             currentCommand+=character;
             write(STDOUT_FILENO, &character, 1);
         }
-    }
+    }//while loop
 
 	return 0;
 }
